0c0c918743a522393afcade5887b0d0cd52247f4
crazylogic03 <saithrishul2006@gmail.com>
2026-02-06 15:30:28 +0530

    Handled Errors in Auth Middleware

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,13 +1,18 @@
 const jwt = require("jsonwebtoken");
-const prisma = require("../prisma/client");
-
+const prisma = require("../prismaClient");
 module.exports = async (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
-
     try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) return res.status(401).json({ message: "Unauthorized" });
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await prisma.user.findUnique({ where: { id: decoded.id } });
+        const user = await prisma.user.findUnique({
+            where: { id: decoded.id }
+        });
+
+        if (!user)
+            return res.status(401).json({ message: "Invalid token" });
+
+        req.user = user;
         next();
     } catch {
         res.status(401).json({ message: "Invalid token" });
