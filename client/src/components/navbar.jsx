4436d9f5894b2ad95991e6138699e7ac4da74617
crazylogic03 <saithrishul2006@gmail.com>
2026-02-15 16:54:00 +0530

    Added Pages and Piece of Code With Installing Tailwing CSS

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function Navbar() {
+
+    return (
+        <>
+            <div>
+                YoLo
+            </div>
+            <div>
+                <p>Home</p>
+                <p>My Trips</p>
+                <p></p>
+            </div>
+        </>
+    );
+}
